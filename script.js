9265c3581897f53e458deea38233627a01e1f82e
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-03-19 23:05:25 +0300

    feat: add smooth scrolling and auto change menu

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menu = document.getElementById('menu');
 const portfolioImages = document.querySelector('.portfolio__images');
 const portfolioNav = document.getElementById('portfolioNav');
+const ANCHORS = document.querySelectorAll('a[href*="#"]')
+const header = document.querySelector('.header');
+const home = document.querySelector('#menu-home');
 console.log(menu);
 console.log(portfolioNav);
 console.log(portfolioImages);
@@ -9,12 +12,57 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+const removeMenuItem = () => {
+  menu.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+}
 
 //-----------menu--------//
+window.addEventListener('scroll', () => {
+ if (window.pageYOffset > 70) {
+   header.classList.add('header-fixed');
+   if (window.pageYOffset > 70 && window.pageYOffset < 400) {
+     removeMenuItem();
+     document.querySelector('#menu-home').classList.add('active');
+   }
+   if (window.pageYOffset > 70 && window.pageYOffset > 400) {
+     removeMenuItem();
+     document.querySelector('#menu-services').classList.add('active');
+   }
+   if (window.pageYOffset > 70 && window.pageYOffset > 900) {
+     removeMenuItem();
+     document.querySelector('#menu-portfolio').classList.add('active');
+   }
+   if (window.pageYOffset > 70 && window.pageYOffset > 1800) {
+     removeMenuItem();
+     document.querySelector('#menu-about').classList.add('active');
+   }
+   if (window.pageYOffset > 70 && window.pageYOffset > 2400) {
+     removeMenuItem();
+     document.querySelector('#menu-contact').classList.add('active');
+   }
+ }
+ else {
+   header.classList.remove('header-fixed');
+ }
+});
+
+
 menu.addEventListener('click', (event) => {
   menu.querySelectorAll('a').forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
 })
+
+////-------smooth scrolling---//////
+for (let anchor of ANCHORS) {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        })
+    })
+}
 
 //-----------interaction with pictures-----//
 portfolioImages.addEventListener('click', (event) => {
@@ -29,7 +77,7 @@ portfolioImages.addEventListener('click', (event) => {
 }
 });
 
-///------------------ tab switching  -------------
+///------------------ tab switching  -------------///
 portfolioNav.addEventListener('click', (event) => {
   if(event.target.classList.value.includes('active') === true) return;
 
